1a6de875ca4fa4038ce7c32f8c5909e7ce2ba833
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-07-31 14:22:35 +0530

    Delete src/next.d.ts

--- src/next.d.ts
+++
@@ -1,8 +0,0 @@
-import 'next';
-
-declare module 'next' {
-  export type PageProps = {
-    params: { [key: string]: string };
-    searchParams?: { [key: string]: string | string[] | undefined };
-  };
-}
