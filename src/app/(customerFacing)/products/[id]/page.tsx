b0d6ca8534afd4c03533f9c4e06d238e4429586d
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-07-31 13:41:08 +0530

    Update_new page.tsx

--- src/app/(customerFacing)/products/[id]/page.tsx
+++ src/app/(customerFacing)/products/[id]/page.tsx
@@ -1,10 +1,10 @@
 import Purchase from "./Purchase";
+import { PageProps } from 'next/app';
+
 export default function PurchasePage({
   params,
 }: {
   params: { id: string };
-}){
-  return (
-    <Purchase id = {params.id} />
-  )
+}) {
+  return <Purchase id={params.id} />;
 }
