43e20571de79faca5e030c1dfdc2ff9e46646145
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-07-31 23:16:35 +0530

    Update page.tsx

--- src/app/(customerFacing)/products/[id]/page.tsx
+++ src/app/(customerFacing)/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 // src/app/(customerFacing)/products/[id]/page.tsx
 import Purchase from "./Purchase";
 
-// Remove all custom type extensions and use simple props
+// Use inline type definition without extending anything
 export default function Page({
   params,
 }: {
