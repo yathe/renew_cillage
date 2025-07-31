5ead39e5cfef01da6bc973d660599ed622199a9c
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-07-31 21:45:14 +0530

    Update next.d.ts

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,8 +1,9 @@
+// src/types/next.d.ts
 import 'next';
 
 declare module 'next' {
-  type PageProps = {
-    params: Record<string, string>;
+  type PageParams<T = {}> = {
+    params: T;
     searchParams?: Record<string, string | string[] | undefined>;
   };
 }
